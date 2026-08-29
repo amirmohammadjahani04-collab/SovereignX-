cebfe982649823b7f7098d03bdab0202e9db1fe1
Amirmohammad Jahani <294794392+amirmohammadjahani04-collab@users.noreply.github.com>
2026-08-29 22:22:36 +0330

    Remove kernel/bakOS_boot.asm per owner request

--- kernel/bakOS_boot.asm
+++ kernel/bakOS_boot.asm
@@ -1,21 +1,2 @@
-BITS 16
-ORG 0x1000
-
-start:
-    mov si, msg
-
-print:
-    lodsb
-    or al, al
-    jz halt
-
-    mov ah, 0x0E
-    int 0x10
-    jmp print
-
-halt:
-    cli
-    hlt
-    jmp halt
-
-msg db "bakOS Kernel Running!", 0
+; DELETED by repository owner request on 2026-08-29
+; original kernel/bakOS_boot.asm removed in-place
