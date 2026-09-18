40490fec27b5137189c5bc0c5769a5d70ee16303
Amirmohammad Jahani <294794392+amirmohammadjahani04-collab@users.noreply.github.com>
2026-09-19 00:36:54 +0330

    Delete text_render.h

--- text_render.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEXT_RENDER_H
-#define TEXT_RENDER_H
-#include <stdint.h>
-
-void draw_char(int x, int y, char c, uint32_t color);
-void draw_string(int x, int y, const char *s, uint32_t color);
-
-#endif
